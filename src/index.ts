9c298a452b201549366269a99c22e9631b042d65
0xrutvij <rutvij.shah96@gmail.com>
2022-07-03 15:25:53 -0700

    specify node version

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
 import { ApolloServer } from "apollo-server";
 import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 
-import { context } from "./context";
 import { schema } from "./schema";
-
+import { context } from "./context";
 
 export const server = new ApolloServer({
-  schema,
-  context,
-  introspection: true,
-  plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    schema,
+    context,
+    introspection: true,                                      // 1
+    plugins: [ApolloServerPluginLandingPageLocalDefault()],   // 2
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;                        // 3
 
-server.listen({port}).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+server.listen({ port }).then(({ url }) => {
+    console.log(`🚀  Server  ready at ${url}`);
 });
